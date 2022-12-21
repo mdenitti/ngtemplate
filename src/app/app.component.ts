0f834d3632e47c997a11da2fcd11677d9726865e
mdenitti <massimodn77@gmail.com>
2022-12-21 11:20:58 +0100

    Integration of a template (free bootstrap)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngtemplate';
+  items = [
+    {title: 'Devices',
+    detail:'This is the details of the devices'},
+    {title: 'Users',
+    detail:'This is the details of the users'}
+  ];
 }
